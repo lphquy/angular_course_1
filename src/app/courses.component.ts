088327557148df0ae959e8d6b742c5c835bb6d92
DESKTOP-T8H3PUN\lphqu <lphquy@gmail.com>
2020-11-16 06:54:00 +0700

    directive: manipulate DOM element. Added using directive ngFor to manipulate DOM.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'    
+    template: `
+        <h2>{{ "title: " + getTitle() }}</h2>
+        <ul *ngFor="let course of courses">
+            {{ course }}
+        </ul>
+    `    
 })
 export class CoursesComponent {
+    title = "List of courses"
+    courses = [
+        'course1',
+        'course2',
+        'course3'
+    ]
 
+    getTitle() {
+        return this.title;
+    }
 }
