4774bd96fc1974dad9fd867f39fac02a2fc97dd1
DESKTOP-T8H3PUN\lphqu <lphquy@gmail.com>
2020-11-16 06:40:48 +0700

    added courseComponent by generate with angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
