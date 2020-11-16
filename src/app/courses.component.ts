0d408cb28551b59e374d17e94d297d3c3437d199
DESKTOP-T8H3PUN\lphqu <lphquy@gmail.com>
2020-11-16 23:05:23 +0700

    added numberOfCourses using. *ngIf

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,6 +5,12 @@ import { CoursesService } from './courses.service';
     selector: 'courses',
     template: `
         <h2>{{ "title: " + getTitle() }}</h2>
+
+        <div *ngIf="numberOfCourses === 1; else plural">
+            <h3>{{numberOfCourses + " Author"}}</h3>
+        </div>
+        <ng-template #plural><h3>{{numberOfCourses + " Authors"}}</h3></ng-template>
+
         <ul *ngFor="let course of courses">
             {{ course }}
         </ul>
@@ -13,6 +19,7 @@ import { CoursesService } from './courses.service';
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    numberOfCourses;
 
     getTitle() {
         return this.title;
@@ -20,5 +27,6 @@ export class CoursesComponent {
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
+        this.numberOfCourses = this.courses.length;
     }
 }
