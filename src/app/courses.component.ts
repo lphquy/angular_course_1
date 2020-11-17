64c4cb5eb9f032176c47122bc290075bf75f575a
DESKTOP-T8H3PUN\lphqu <lphquy@gmail.com>
2020-11-17 07:05:07 +0700

    update data to new branch, current source not working

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,29 +4,12 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ "title: " + getTitle() }}</h2>
-
-        <div *ngIf="numberOfCourses === 1; else plural">
-            <h3>{{numberOfCourses + " Author"}}</h3>
-        </div>
-        <ng-template #plural><h3>{{numberOfCourses + " Authors"}}</h3></ng-template>
-
-        <ul *ngFor="let course of courses">
-            {{ course }}
-        </ul>
+        <h2>{{title}}</h2>
+        <image src="{{imageUrl}}"/>   
     `
 })
+
 export class CoursesComponent {
     title = "List of courses";
-    courses;
-    numberOfCourses;
-
-    getTitle() {
-        return this.title;
-    }
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-        this.numberOfCourses = this.courses.length;
-    }
+    imageUrl = "http://lorempixel.com/400/200";
 }
