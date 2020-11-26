2435f69d015759706b2cb845d69b94df572100b6
DESKTOP-T8H3PUN\lphqu <lphquy@gmail.com>
2020-11-27 06:22:33 +0700

    update coding.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,10 +6,16 @@ import { CoursesService } from './courses.service';
     template: `
         <h2>{{title}}</h2>
         <img [src]="imageUrl" />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            <tr>
+        </table>
     `
 })
 
 export class CoursesComponent {
     title = "List of courses";
     imageUrl = "http://lorempixel.com/400/200";
+    colSpan = 2;
 }
